a609ca079cf45fcafba3603af9fc4b42c08af47b
hegazi <yousefhegazi74@gmail.com>
2024-03-03 03:32:49 +0200

    adding gazebo control

--- diff_bot/launch/rsp.launch.py
+++ diff_bot/launch/rsp.launch.py
@@ -40,6 +40,5 @@ def generate_launch_description():
     return LaunchDescription(
         [            
             robot_state_publisher_node,
-            joint_state_publisher_node
         ]
     )
